c856e3b9a37574f644f020e786e4061ff9de2980
Hangbo <zhzhanghangbo@hotmail.com>
2020-02-25 22:40:23 -0500

    Add Singleton and Factory design patterns materials

---
+++ src/DesignPatterns/ExampleFactory.java
@@ -0,0 +1,24 @@
+package DesignPatterns;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+
+public class ExampleFactory {
+
+    private ExampleFactory() {}
+
+    public List<Integer> getList(int typeCode) {
+        if (typeCode == 0) {
+            return null;
+        }
+        if (typeCode == 1) {
+            return new ArrayList<>();
+        }
+        if (typeCode == 2) {
+            return new LinkedList<>();
+        }
+        return null;
+    }
+
+}
---
+++ src/DesignPatterns/SingletonExample.java
@@ -0,0 +1,13 @@
+package DesignPatterns;
+
+public class SingletonExample {
+
+    private static SingletonExample obj;
+
+    private SingletonExample(){}
+
+    public static SingletonExample getInstance() {
+        if (obj == null) obj = new SingletonExample();
+        return obj;
+    }
+}
