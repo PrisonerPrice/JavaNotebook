cd215042496ee5d911ddaf789fb8297494870010
Hangbo <zhzhanghangbo@hotmail.com>
2020-03-03 12:15:52 -0500

    Add a question solved in Kotlin

---
+++ src/SomeProblems/Question1KT.kt
@@ -0,0 +1,59 @@
+package SomeProblems
+
+import java.util.*
+
+fun main(args: Array<String>) {
+    Solution.print(399);
+}
+
+object Solution {
+
+    fun print(n: Int) {
+        val stack = ArrayDeque<LinkedList<Int>>()
+        var digit = 6
+        var remainder = n
+        var minus = 1000000
+        while (minus > 0) {
+            val curr = LinkedList<Int>()
+            while (remainder - minus >= 0) {
+                curr.add(minus)
+                remainder -= minus
+            }
+            digit--
+            if (!curr.isEmpty()) stack.push(curr)
+            minus /= 10
+        }
+        while (!stack.isEmpty()) {
+            val small = stack.pop()
+            if (!stack.isEmpty()) {
+                val large = stack.pop()
+                val lenS = small.size
+                val lenL = large.size
+                var i = 0
+                while (i < lenS && i < lenL) {
+                    large[i] = small[i] + large[i]
+                    i++
+                }
+                if (lenS > lenL) {
+                    for (x in i until lenS) large.add(small[i])
+                }
+                stack.push(large)
+            } else {
+                stack.push(small)
+                break
+            }
+        }
+        val result = StringBuilder()
+        var count = 0
+        while (!stack.isEmpty()) {
+            val curr = stack.pop()
+            count += curr.size
+            for (x in curr.size - 1 downTo 0) {
+                result.append(curr[x])
+                result.append(" ")
+            }
+        }
+        println(count)
+        println(result.toString().trim())
+    }
+}
